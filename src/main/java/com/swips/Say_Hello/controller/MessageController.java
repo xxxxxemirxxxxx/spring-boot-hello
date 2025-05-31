3d58af7e90c69427953f93fbf4b904eaaf48e3ba
xxxxxemirxxxxx <emirhadziomerovicaws@gmail.com>
2025-05-31 17:34:26 +0200

    first commit

---
+++ src/main/java/com/swips/Say_Hello/controller/MessageController.java
@@ -0,0 +1,17 @@
+package com.swips.Say_Hello.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+
+    @GetMapping("/hi")
+    public ResponseEntity<String> sayHi() {
+        return ResponseEntity.ok("Hello, from the Spring Boot rest controller");
+    }
+
+}
